e27d42e7df7c37bb345140c3a5edaa8e3d4f6bfd
James Massara <james_massara@intuit.com>
2016-12-22 14:32:24 -0800

    usps: Clean up XML structs
    Reduce the number of XML structs required.
    
    For `cityStateLookupResponse`, use an embedded struct to represent `ZipCode`.
    
    Also, rename `Error` to `apiError` and leave it unexported.  No reason to have it exported.

--- usps.go
+++ usps.go
@@ -2,7 +2,7 @@ package usps
 
 import (
 	"encoding/xml"
-	"errors"
+	"fmt"
 	"io/ioutil"
 	"net/http"
 	"net/url"
@@ -17,10 +17,10 @@ const (
 )
 
 //ValidateZip returns non empty Response if successful
-func (a API) ValidateZip(zipCode string) (*Response, error) {
+func (a API) ValidateZip(zipCode string) (*CityStateLookupResponse, error) {
 	client := a.HTTPClient
-	r := &Response{}
-	e := &Error{}
+	r := &CityStateLookupResponse{}
+	e := &apiError{}
 
 	xmlVals := "<CityStateLookupRequest USERID='" + a.Credentials + "'><ZipCode ID='0'> <Zip5>" + zipCode + "</Zip5></ZipCode></CityStateLookupRequest>"
 
@@ -46,7 +46,7 @@ func (a API) ValidateZip(zipCode string) (*Response, error) {
 	body, readerr := ioutil.ReadAll(resp.Body)
 	if readerr == nil {
 		//load xml object
-		if xmlerr := xml.Unmarshal(body, &r.CityStateLookupResponse); xmlerr != nil {
+		if xmlerr := xml.Unmarshal(body, &r); xmlerr != nil {
 			return r, xmlerr
 		}
 	} else {
@@ -54,14 +54,11 @@ func (a API) ValidateZip(zipCode string) (*Response, error) {
 	}
 
 	//Handle USPS error messages
-	if r.CityStateLookupResponse.ZipCode == nil {
-		xml.Unmarshal(body, &e)
-		err := errors.New(e.Description.Text)
-		return r, err
-	}
-	if r.CityStateLookupResponse.ZipCode.Error != nil {
-		err := errors.New(r.CityStateLookupResponse.ZipCode.Error.Description.Text)
-		return r, err
+	if r.ZipCode.ID == "" {
+		if err := xml.Unmarshal(body, &e); err != nil {
+			return r, nil
+		}
+		return r, e
 	}
 
 	return r, nil
@@ -86,68 +83,27 @@ type API struct {
 	HTTPClient  *http.Client
 }
 
-//Response for full response
-type Response struct {
-	CityStateLookupResponse *CityStateLookupResponse `xml:"CityStateLookupResponse,omitempty" json:"response,omitempty"`
-}
-
-//CityStateLookupResponse for success/error response
+// CityStateLookupResponse is the XML response for the CityStateLookupRequest
+// type.
 type CityStateLookupResponse struct {
-	ZipCode *ZipCode `xml:"ZipCode,omitempty" json:"zip_code,omitempty"`
+	XMLName xml.Name `xml:"CityStateLookupResponse" json:"-"`
+	ZipCode struct {
+		ID    string `xml:"ID,attr,omitempty" json:"id,omitempty"`
+		Zip5  string `xml:"Zip5,omitempty" json:"zip5,omitempty"`
+		City  string `xml:"City,omitempty" json:"city,omitempty"`
+		State string `xml:"State,omitempty" json:"state,omitempty"`
+	} `xml:"ZipCode,omitempty" json:"zipcode,omitempty"`
 }
 
-//ZipCode for success/error response
-type ZipCode struct {
-	ID    string `xml:"ID,attr"  json:"id,"`
-	City  *City  `xml:"City,omitempty" json:"city,omitempty"`
-	State *State `xml:"State,omitempty" json:"state,omitempty"`
-	Zip5  *Zip5  `xml:"Zip5,omitempty" json:"zip5,omitempty"`
-	Error *Error `xml:"Error,omitempty" json:"error,omitempty"`
+// apiError is the XML structure for errors returned by the API.
+type apiError struct {
+	XMLName     xml.Name `xml:"Error" json:"-"`
+	Number      string   `xml:"Number,omitempty" json:"number"`
+	Description string   `xml:"Description,omitempty" json:"description"`
+	Source      string   `xml:"Source,omitempty" json:"source"`
 }
 
-//Zip5 for successful response
-type Zip5 struct {
-	Text string `xml:",chardata" json:",omitempty"`
-}
-
-//City for successful response
-type City struct {
-	Text string `xml:",chardata" json:",omitempty"`
-}
-
-//State for successful response
-type State struct {
-	Text string `xml:",chardata" json:",omitempty"`
-}
-
-//Error for error response
-type Error struct {
-	Description *Description `xml:" Description,omitempty" json:"description,omitempty"`
-	HelpContext *HelpContext `xml:" HelpContext,omitempty" json:"help_context,omitempty"`
-	HelpFile    *HelpFile    `xml:" HelpFile,omitempty" json:"help_file,omitempty"`
-	Number      *Number      `xml:" Number,omitempty" json:"number,omitempty"`
-	Source      *Source      `xml:" Source,omitempty" json:"source,omitempty"`
-}
-
-//Description for for error response
-type Description struct {
-	Text string `xml:",chardata" json:",omitempty"`
-}
-
-//HelpContext for error response
-type HelpContext struct {
-}
-
-//HelpFile for error response
-type HelpFile struct {
-}
-
-//Number for error response
-type Number struct {
-	Text string `xml:",chardata" json:",omitempty"`
-}
-
-//Source for error response
-type Source struct {
-	Text string `xml:",chardata" json:",omitempty"`
+// Implement the error interface.
+func (e *apiError) Error() string {
+	return fmt.Sprintf("number: %s, source: %s; %s", e.Number, e.Source, e.Description)
 }
